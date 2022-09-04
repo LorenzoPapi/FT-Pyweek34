c1f57ebe3d9c5641c417d83410175526606e48b6
LorenzoPapi <42911751+LorenzoPapi@users.noreply.github.com>
2022-09-04 19:45:57 +0200

    Folders

---
+++ game/player.py
@@ -0,0 +1,33 @@
+import pygame
+
+def clamp(v, maxv, minv):
+    return min(max(v, minv), maxv)
+
+
+class Player:
+    def __init__(self):
+        self.rect = pygame.Rect(415, 450, 50, 100)
+        self.gravity_y = 0
+        self.friction = 1
+        self.frames = 0
+        self.jumpf = 0
+
+    def move_player(self, event):
+        key = event.key
+        if (key == pygame.K_LEFT):
+            pass    
+        if (key == pygame.K_RIGHT):
+            pass
+        if (key == pygame.K_SPACE and self.rect.y >= 450):
+            self.gravity_y = -7
+            self.friction = 1
+            self.jumpf = self.frames
+
+    def update_player(self):
+        self.rect.y += self.gravity_y * self.friction
+        self.rect.y = clamp(self.rect.y, 450, 0)
+        if (self.frames == (self.jumpf + 10)):
+            self.gravity_y = 7
+            self.friction = 0.5
+            self.jumpf = 0
+        self.frames += 1
