5c002762c244199589f3f6d4d76392873533d9b6
LorenzoPapi <42911751+LorenzoPapi@users.noreply.github.com>
2022-09-04 19:31:21 +0200

    Jump, Bullets with X

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from turtle import update
 import pygame
-import game.player as player
+from game.player import Player
 from sys import exit
 
 pygame.init()
+player = Player()
 
 colors = {"RED": (255, 64, 64), "WHITE": (255, 255, 255)}
 
@@ -15,23 +17,24 @@ bullets = []
 game_true = True
 
 def generateBullet(size = 50):
-    bullet_obj = {"rect": pygame.Rect(player["rect"].x, player["rect"].y, size, size), "gravity": 7}
+    bullet_obj = {"rect": pygame.Rect(player.rect.x, player.rect.y, size, size), "gravity": 7}
     bullets.append(bullet_obj)
     return bullet_obj
 
 while True:
     window.fill('Black')
     pygame.draw.ellipse(window, colors["RED"], planet)
-    pygame.draw.rect(window, colors["WHITE"], player["rect"])
+    pygame.draw.rect(window, colors["WHITE"], player.rect)
+    player.update_player()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_x:
                 new_bullet = generateBullet()
         elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
-            player["move_player"](event)
+            player.move_player(event)
 
     for obj in bullets:
         pygame.draw.rect(window, colors["RED"], obj["rect"])
