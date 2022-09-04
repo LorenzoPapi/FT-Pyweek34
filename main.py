636e333aba315b9a7adb9aeebc41f159274af0bf
st4rlollo <112164422+st4rlollo@users.noreply.github.com>
2022-09-05 00:24:43 +0200

    imported planet & projectile imgs

--- main.py
+++ main.py
@@ -12,18 +12,22 @@ window = pygame.display.set_mode((900, 900))
 caption = pygame.display.set_caption('hello')
 clock = pygame.time.Clock()
 
-planet = pygame.Rect(-315, 550, 1500, 1500)
+planet_img = pygame.image.load('FT-Pyweek34/assets/textures/red_planet.png').convert_alpha()
+planet_img = pygame.transform.rotozoom(planet_img, 0, 1.5)
+projectile_img = pygame.image.load('FT-Pyweek34/assets/textures/projectile.png').convert_alpha()
+projectile_img = pygame.transform.rotozoom(projectile_img, 0, 2.5)
+planet = planet_img.get_rect(center = (450, 1450))
 bullets = []
 game_true = True
 
 def generateBullet(size = 50):
-    bullet_obj = {"rect": pygame.Rect(player.rect.x, player.rect.y, size, size), "gravity": 7}
+    bullet_obj = {"rect": projectile_img.get_rect(topleft = (player.rect.x, player.rect.y)), "gravity": 7}
     bullets.append(bullet_obj)
     return bullet_obj
 
 while True:
     window.fill('Black')
-    pygame.draw.ellipse(window, colors["RED"], planet)
+    window.blit(planet_img, planet)
     pygame.draw.rect(window, colors["WHITE"], player.rect)
     player.update_player()
     for event in pygame.event.get():
@@ -37,7 +41,7 @@ while True:
             player.move_player(event)
 
     for obj in bullets:
-        pygame.draw.rect(window, colors["RED"], obj["rect"])
+        window.blit(projectile_img, obj["rect"])
         obj["rect"].x += obj["gravity"]
 
     pygame.display.flip()
