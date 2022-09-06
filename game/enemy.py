6741b7c23192f0dbde4ad780280199c089b3ae34
st4rlollo <112164422+st4rlollo@users.noreply.github.com>
2022-09-06 12:21:09 +0200

    enemies

---
+++ game/enemy.py
@@ -0,0 +1,24 @@
+#import pygame
+#from .utils import *
+#from random import randint
+#from main import player, planet 
+
+#pizza_enemy = pygame.image.load('assets/textures/pizza_enemy.png')
+#apple_enemy = pygame.image.load('assets/textures/apple.png')
+#macaroni_enemy = pygame.image.load('assets/textures/macaroni.png')
+#enemy_list = [pizza_enemy, apple_enemy, macaroni_enemy]
+#random_enemy = enemy_list[randint(0,3)]
+#enemies = []
+#def generate_enemy(size=16):
+    #enemy = SuperSprite(window, pygame.image.load("assets/textures/projectile.png").convert_alpha())
+    #enemies.append({"sprite": enemy, "angle": 0, "start": (planet.size[0]/2 + player.rect.size[0], planet.size[1]/2 + player.rect.size[1] / 2 - player.rect.y + player.ground_y)}) #(planet.size[0]/2 + player.rect.size[0], -planet.size[1]/2 + player.rect.bottom)
+    #return enemy
+
+
+        
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import math
 import pygame
 from game.player import *
 from game.utils import *
+from random import randint
 
 pygame.init()
 colors = {"RED": (255, 64, 64), "WHITE": (255, 255, 255)}
@@ -25,10 +26,12 @@ class Planet(SuperSprite):
 
 planet = Planet(window)
 player = Player(window, planet)
+#enemy = Enemy(window, planet, randint(0,3))
 
 caption = pygame.display.set_caption('Catch the Ketchup!')
 clock = pygame.time.Clock()
 bullets = []
+enemies = []
 running = True
 
 def key_handler():
@@ -47,12 +50,27 @@ def event_handler():
 
 
 def generate_bullet(size=16):
-    bullet = SuperSprite(window, pygame.image.load("assets/textures/projectile.png").convert_alpha())
+    
+    bullet = SuperSprite(window, pygame.image.load('assets/textures/projectile.png').convert_alpha())
     bullets.append({"sprite": bullet, "angle": 0, "start": (planet.size[0]/2 + player.rect.size[0], planet.size[1]/2 + player.rect.size[1] / 2 - player.rect.y + player.ground_y)}) #(planet.size[0]/2 + player.rect.size[0], -planet.size[1]/2 + player.rect.bottom)
     return bullet
 
+enemies = []
+apple_enemy = pygame.image.load('assets/textures/apple.png')
+macaroni_enemy = pygame.image.load('assets/textures/macaroni.png')
+pizza_enemy = pygame.image.load('assets/textures/pizza_enemy.png')
+enemy_sprites = [apple_enemy, macaroni_enemy, pizza_enemy]
+index = enemy_sprites[randint(0,2)]
+
+def generate_enemy(size=40):
+    index = enemy_sprites[randint(0,2)]
+    enemy = SuperSprite(window, index.convert_alpha())
+    enemies.append({"sprite": enemy, "angle": 0.4, "start": (planet.size[0]/2 + player.rect.size[0], planet.size[1]/2 + player.rect.size[1] / 2 - player.rect.y + player.ground_y)}) #(planet.size[0]/2 + player.rect.size[0], -planet.size[1]/2 + player.rect.bottom)
+    return enemy
+generate_enemy()
 while running:
     window.fill('Black')
+    event_handler()
     for obj in bullets:
         bullet = obj["sprite"]
         bullet.image = pygame.transform.rotozoom(bullet.orig_image, math.degrees(-obj["angle"]), 1)
@@ -61,15 +79,29 @@ while running:
         window.blit(bullet.image, bullet.rect) 
         pygame.draw.rect(window, (255, 64, 255), bullet.rect, 2)
         obj["angle"] += 0.01
-        
         if (isOutsideSurface(window, bullet.rect.topleft)):
             bullets.remove(obj)
+    for obj in enemies:
+        enemy = obj["sprite"]
+        enemy.image = pygame.transform.rotozoom(enemy.orig_image, math.degrees(-obj["angle"]), 1)
+        enemy.rect = enemy.image.get_rect(center=(planet.center[0] + (obj["start"][0]) * math.sin(obj["angle"]), planet.center[1] - (obj["start"][1]) * math.cos(obj["angle"])))
+        window.blit(enemy.image, enemy.rect) 
+        obj["angle"] += 0.005
+        if pygame.sprite.collide_rect(enemy, player):
+            print("a")
+            pygame.quit()
+        for obj2 in bullets:
+            if pygame.sprite.collide_rect(enemy,obj2["sprite"]):
+                enemies.remove(obj)
+                bullets.remove(obj2)
+
+    
+        #if pygame.sprite.collide_rect(enemy,bullet)
 
     planet.update_planet()
     player.update_player()
 
     pygame.display.flip()
     key_handler()
-    event_handler()
     clock.tick(60)
     
