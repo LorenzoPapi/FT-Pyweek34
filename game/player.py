defceb3d02573d045b2e85faa1097cdb5d321447
LorenzoPapi <topolino123654@gmail.com>
2022-09-06 12:21:51 +0200

    Cleanup + Refactor

--- game/player.py
+++ game/player.py
@@ -3,38 +3,64 @@ import os
 from .utils import *
 
 class Player(SuperSprite):
-    def __init__(self, window, planet):
-        super().__init__(window, pygame.image.load(os.path.join("assets", "textures", "player_2.png")).convert_alpha())
+    def __init__(self, planet):
+        super().__init__(pygame.image.load(asset_path("texture", "player_2.png")))
+        self.images = [
+            self.orig_image,
+            pygame.transform.flip(self.orig_image, True, False)
+        ]
+        
+        self.planet = planet
         self.ground_y = planet.rect.topleft[1] - self.image.get_size()[1]
-        self.rect = self.image.get_rect(center=(self.window.get_width() / 2, self.ground_y))
+        self.rect = self.image.get_rect(center=(game_window.get_width() / 2, self.ground_y))
+        
         self.gravity_y = 7
         self.friction = 1
         self.frames = 0
         self.jumpf = 0
+        self.bulletf = 0
         self.dir = 0
+        self.bullets = []
+    
+    def key_handler(self, event):
+        if (event.type == pygame.KEYDOWN and event.key == pygame.K_x):
+            if (self.frames >= (self.bulletf + 10) or self.bulletf == 0):
+                self.shoot()
 
     def move_player(self, pressed):
-        #if (pressed[pygame.K_LEFT] and self.dir != 1):
-        #    self.dir = 1
-        #    self.image = pygame.transform.flip(self.image.copy(), True, False)
-        #elif (pressed[pygame.K_RIGHT] and self.dir != 0):
-        #    self.dir = 0
-        #    self.image = pygame.transform.flip(self.image.copy(), True, False)
+        self.orig_image = self.images[self.dir]
+        if (pressed[pygame.K_LEFT] and not pressed[pygame.K_RIGHT] and self.dir != 1):
+            self.dir = 1
+        elif (pressed[pygame.K_RIGHT] and not pressed[pygame.K_LEFT] and self.dir != 0):
+            self.dir = 0
         if (pressed[pygame.K_SPACE] and self.rect.y >= self.ground_y and self.gravity_y == 7):
-            self.gravity_y = -7
-            self.friction = 1
-            self.jumpf = self.frames
+            self._jump(False)
         elif (not pressed[pygame.K_SPACE] and self.gravity_y == -7):
-            self.gravity_y = 7
-            self.friction = 1.1
-            self.jumpf = 0
+            self._jump(True)
+
+    def _jump(self, down):
+        self.gravity_y = (1 if down else -1) * 7
+        self.friction = 1.1 if down else 1
+        self.jumpf = 0 if down else self.frames
 
     def update_player(self):
-        self.rect.y += self.gravity_y * self.friction
-        self.rect.y = clamp(self.rect.y, self.ground_y, 0)
+        self.rect.y = clamp(self.rect.y + self.gravity_y * self.friction, self.ground_y, 0)
         if (self.frames == (self.jumpf + 20)):
-            self.gravity_y = 7
-            self.friction = 1.1
-            self.jumpf = 0
+            self._jump(True)
+        
+        for obj in self.bullets:
+            bullet : SuperSprite = obj["sprite"]
+            bullet.rect = bullet.image.get_rect(center=(self.planet.center[0] + (obj["start"][0]) * math.sin(bullet.angle), self.planet.center[1] - (obj["start"][1]) * math.cos(bullet.angle)))
+            bullet.update()
+            bullet.angle += (1 if obj["dir"] == 0 else -1) * 0.04
+            if (isOutsideSurface(game_window, bullet.rect.topleft)):
+                self.bullets.remove(obj)
+
         self.frames += 1
         self.update()
+    
+    def shoot(self):
+        bullet = SuperSprite(pygame.image.load(asset_path("texture", "projectile.png")))
+        self.bullets.append({"sprite": bullet, "dir": self.dir, "start": (self.planet.radius + self.rect.size[0], self.planet.radius + self.rect.size[1] / 2 - self.rect.y + self.ground_y)})
+        self.bulletf = self.frames
+
--- game/utils.py
+++ game/utils.py
@@ -1,30 +1,43 @@
+import math
 import pygame
+from pygame.locals import *
+import os
 
-window = pygame.display.set_mode((1200, 900))
+game_window = pygame.display.set_mode((1200, 900), DOUBLEBUF, 16)
+colors = {"RED": (255, 64, 64), "WHITE": (255, 255, 255)}
 
 def clamp(v, maxv, minv):
     return min(max(v, minv), maxv)
 
-def isOutsideSurface(window, point):
-    h, w = window.get_height(), window.get_width()
+def isOutsideSurface(surf : pygame.Surface, point):
+    h, w = surf.get_height(), surf.get_width()
     return (point[0] < 0 or point[0] > w) or (point[1] < 0 or point[1] > h)
 
 # https://stackoverflow.com/questions/4183208/how-do-i-rotate-an-image-around-its-center-using-pygame
-def rot_center(image, angle, center):
+def rot_center(image : pygame.Surface, angle, center):
     rotated = pygame.transform.rotate(image, angle)
     return rotated, rotated.get_rect(center = image.get_rect(center = center).center)
 
+def asset_path(type, file):
+    return os.path.join("assets", type + "s", file)
+
 class SuperSprite(pygame.sprite.Sprite):
-    def __init__(self, window, image):
+    def __init__(self, image : pygame.Surface):
         super().__init__()
-        self.window = window
-        self.image = image
-        self.orig_image = image
+        self.image = image.convert_alpha()
         self.rect = self.image.get_rect()
         self.angle = 0
         self.scale = 1
+        self.orig_image = self.image
+        self._oldS = self.scale
+        self._oldA = self.angle
+        self._oldOI = self.orig_image
     
     def update(self):
-        self.image = pygame.transform.rotozoom(self.orig_image, self.angle, self.scale)
-        self.window.blit(self.image, self.rect)
-        pygame.draw.rect(self.window, (255, 64, 255), self.rect, 2)
+        if (self._oldS != self.scale or self._oldA != self.angle or self._oldOI != self.orig_image):
+            self.image = pygame.transform.rotozoom(self.orig_image, math.degrees(-self.angle), self.scale)
+        game_window.blit(self.image, self.rect)
+        pygame.draw.rect(game_window, (255, 64, 255), self.rect, 2)
+        self._oldS = self.scale
+        self._oldA = self.angle
+        self._oldOI = self.orig_image
