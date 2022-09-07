542d3118066140dd58a540528c34117db835499b
st4rlollo <112164422+st4rlollo@users.noreply.github.com>
2022-09-07 20:24:36 +0200

    trying title screen

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ pygame.init()
 player = Player()
 
 caption = pygame.display.set_caption('Catch the Ketchup!')
+title_screen = pygame.image.load('assets/textures/home_screen_final.gif')
 clock = pygame.time.Clock()
-running = 1
+running = 0
 
 def event_handler():
     global running
@@ -17,12 +18,20 @@ def event_handler():
             pygame.quit()
         elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
             player.key_handler(event)
+        if not running:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                running = 1
 
 while running:
-    game_window.fill('Black')
-    player.update_player()
+    if running:
+        game_window.fill('Black')
+        player.update_player()
 
-    pygame.display.update()
-    event_handler()
-    clock.tick(60)
+        pygame.display.update()
+        event_handler()
+        clock.tick(60)
+    else:
+        game_window.blit(title_screen, (0,0))
+        event_handler()
+
     
